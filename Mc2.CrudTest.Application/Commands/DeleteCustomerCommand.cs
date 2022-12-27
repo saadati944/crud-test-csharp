8a3ed7c64472d8b7e70b25bf80660ff90c60f415
saadati944 <saadati944@gmail.com>
2022-12-27 17:14:06 +0330

    Add delete customer endpoint

---
+++ Mc2.CrudTest.Application/Commands/DeleteCustomerCommand.cs
@@ -0,0 +1,14 @@
+﻿using Mc2.CrudTest.Domain.CustomerAggregate;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Commands;
+
+public class DeleteCustomerCommand : IRequest
+{
+    public Guid ID { get; set; }
+}
---
+++ Mc2.CrudTest.Application/Handlers/DeleteCustomerHandler.cs
@@ -0,0 +1,33 @@
+﻿using Mc2.CrudTest.Application.Commands;
+using Mc2.CrudTest.Application.Exceptions;
+using Mc2.CrudTest.Domain.Abstractions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Handlers;
+
+public class DeleteCustomerHandler : IRequestHandler<DeleteCustomerCommand>
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public DeleteCustomerHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public Task<Unit> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+    {
+        var customer = _customerRepository.GetCustomerByID(request.ID);
+        
+        if (customer is null)
+            throw new CustomerNotFoundException();
+
+        _customerRepository.DeleteCustomer(customer);
+
+        return Task.FromResult(Unit.Value);
+    }
+}
