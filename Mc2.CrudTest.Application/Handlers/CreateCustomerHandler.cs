2c0dd28c4119ffad27ecf07a5bbb993359c1e45d
saadati944 <saadati944@gmail.com>
2022-12-27 15:12:53 +0330

    Add MediatR and create customer handler

---
+++ Mc2.CrudTest.Application/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿using Mc2.CrudTest.Application.Commands;
+using Mc2.CrudTest.Domain.CustomerAggregate;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Handlers;
+
+public class CreateCustomerHandler : IRequestHandler<CreateCustomerCommand, Customer>
+{
+    public Task<Customer> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        var customer = Customer.Create(request.Firstname, request.Lastname, request.DateOfBirth, request.PhoneNumber, request.EmailAddress, request.BankAccountNumber);
+        return Task.FromResult(customer);
+    }
+}
