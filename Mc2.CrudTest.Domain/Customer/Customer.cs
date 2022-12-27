aa1d5e8ffdb0f8b167db76f903a2c1ae2767b938
saadati944 <saadati944@gmail.com>
2022-12-27 09:33:11 +0330

    Add customer domain model

---
+++ Mc2.CrudTest.Domain/Customer/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Domain.Customer
+{
+    public class Customer
+    {
+        // There may be an entity in the infrastructure layer which stores the actual data in the database
+        // but for simplicity I just use my domain model as an entity.
+        public Guid ID { get; set; }
+
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccountNumber { get; set; }
+    }
+}
