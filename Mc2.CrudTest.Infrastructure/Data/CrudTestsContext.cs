73d85a48cb556481cb7c73d8de35c80f8ad891f6
saadati944 <saadati944@gmail.com>
2022-12-27 10:43:21 +0330

    Add EntityFrameworkCore and database configurations

---
+++ Mc2.CrudTest.Infrastructure/Data/CrudTestsContext.cs
@@ -0,0 +1,22 @@
+﻿using Mc2.CrudTest.Domain.Customer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Infrastructure.Data;
+
+public class CrudTestsContext : DbContext
+{
+    public CrudTestsContext() : base()
+    {
+    }
+
+    public CrudTestsContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+}
