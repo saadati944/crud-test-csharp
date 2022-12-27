4a4f3f3e1d091975d6d681634781a9e1c6dc9459
saadati944 <saadati944@gmail.com>
2022-12-27 10:12:56 +0330

    Add create customer endpoint

---
+++ Mc2.CrudTest.Api/Endpoints/CustomerEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using Mc2.CrudTest.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.Api.Endpoints;
+
+public static class CustomerEndpoints
+{
+    public static void MapCustomerEndpoints(this WebApplication app)
+    {
+        app.MapPost("/Customer", CreateCustomer);
+    }
+
+    private static CreateCustomerResponse CreateCustomer([FromBody] CreateCustomerRequest customer)
+    {
+        return new CreateCustomerResponse(Guid.NewGuid(), customer.FirstName, customer.LastName);
+    }
+}
