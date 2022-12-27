3151c1e09e1d0c45a85598bc448ff69c9d550d35
saadati944 <saadati944@gmail.com>
2022-12-27 11:03:08 +0330

    Add customer repository

--- Mc2.CrudTest.Domain/CustomerAggregate/Customer.cs
+++ Mc2.CrudTest.Domain/CustomerAggregate/Customer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mc2.CrudTest.Domain.Customer
+namespace Mc2.CrudTest.Domain.CustomerAggregate
 {
     public class Customer
     {
---
+++ Mc2.CrudTest.Infrastructure/Data/CustomerRepository.cs
@@ -0,0 +1,44 @@
+﻿using Mc2.CrudTest.Domain.Abstractions;
+using Mc2.CrudTest.Domain.CustomerAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Infrastructure.Data;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly CrudTestsContext _context;
+
+    public CustomerRepository(CrudTestsContext context)
+    {
+        _context = context;
+    }
+
+    public void DeleteCustomer(Customer c)
+    {
+        _context.Remove(c);
+    }
+
+    public Customer GetCustomerByID(Guid id)
+    {
+        return _context.Customers.Where(c => c.ID == id).FirstOrDefault();
+    }
+
+    public void InsertCustomer(Customer customer)
+    {
+        _context.Add(customer);
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+
+    public void UpdateCustomer(Customer customer)
+    {
+        _context.Update(customer);
+    }
+}
