df93310b586198f848ef3d7bc7c4b2e6a70e5a23
saadati944 <saadati944@gmail.com>
2022-12-28 22:03:27 +0330

    Add global exception handling

--- Mc2.CrudTest.Api/Program.cs
+++ Mc2.CrudTest.Api/Program.cs
@@ -1,6 +1,8 @@
 using Mc2.CrudTest.Api.Endpoints;
 using Mc2.CrudTest.Application;
 using Mc2.CrudTest.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,20 @@ builder.Services.RegisterDatabase(builder.Configuration.GetConnectionString("Cru
 builder.Services.RegisterServices();
 builder.Services.RegisterMediatR();
 
+// Configure the HTTP request pipeline.
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = Application.Json;
+
+        await context.Response.WriteAsJsonAsync("InternalServerError");
+    });
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
